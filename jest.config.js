9c6c1c2edde69e47878e4b780ae4d2604d5a50b4
NotBadCode <notbadcode.dev@gmail.com>
2025-08-29 12:30:58 +0200

    Update ci/cd

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,20 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'html'],
   coverageDirectory: '<rootDir>/test/coverage',
-  reporters: ['default', ['jest-junit', { outputDirectory: 'reports/junit', outputName: 'results.xml' }]],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports/junit',
+        outputName: 'results.xml',
+        suiteNameTemplate: '{filepath}',
+        classNameTemplate: '{classname} - {title}',
+        titleTemplate: '{title}',
+        ancestorSeparator: ' › ',
+        usePathForSuiteName: 'true',
+      },
+    ],
+  ],
   projects: ['<rootDir>/apps/auth/test/jest.config.js', '<rootDir>/libs/common/test/jest.config.js'],
 };

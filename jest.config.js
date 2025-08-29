607cb132e469921bce547d1990b02ceffd10cfd0
NotBadCode <notbadcode.dev@gmail.com>
2025-08-29 12:19:03 +0200

    Add reporter

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'html'],
   coverageDirectory: '<rootDir>/test/coverage',
+  reporters: ['default', ['jest-junit', { outputDirectory: 'reports/junit', outputName: 'results.xml' }]],
   projects: ['<rootDir>/apps/auth/test/jest.config.js', '<rootDir>/libs/common/test/jest.config.js'],
 };

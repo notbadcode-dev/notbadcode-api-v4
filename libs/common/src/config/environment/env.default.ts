c56dd4cc5dfdb07b1988502847ff5be9863b6680
NotBadCode <notbadcode.dev@gmail.com>
2025-09-02 01:39:42 +0200

    refactor validations

--- libs/common/src/config/environment/env.default.ts
+++ libs/common/src/config/environment/env.default.ts
@@ -30,5 +30,6 @@ export const ENV_DEFAULTS = {
 } as const;
 
 export const AUTH_JWT_SECRET_MIN_LENGTH = 32;
-export const AUTH_JWT_EXPIRES_IN_PATTERN = /^\d+(ms|s|m|h|d)$/;
+export const AUTH_JWT_EXPIRES_IN_PATTERN = /^(\d+)([smhdwy])?$/i;
+
 export const ENV_REDIS_SCHEMES = ['redis', 'rediss'];

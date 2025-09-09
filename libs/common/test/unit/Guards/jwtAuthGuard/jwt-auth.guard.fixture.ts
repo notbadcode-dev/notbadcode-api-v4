945b00609d4941d6c4437eda904875c7fb94e04f
NotBadCode <76620856+notbadcode-dev@users.noreply.github.com>
2025-09-09 22:20:47 +0200

    feat: add JWT auth guard

---
+++ libs/common/test/unit/Guards/jwtAuthGuard/jwt-auth.guard.fixture.ts
@@ -0,0 +1,19 @@
+import { EJwtType } from '@apps/auth/src/infrastructure/jwt/jwt-type.enum';
+
+export class JwtAuthGuardFixture {
+  static readonly secret = 'test-secret';
+
+  static readonly validPayload = {
+    sub: 1,
+    email: 'test@example.com',
+    jti: 'uuid',
+    tokenType: EJwtType.ACCESS,
+  } as const;
+
+  static readonly refreshPayload = {
+    sub: 1,
+    email: 'test@example.com',
+    jti: 'uuid',
+    tokenType: EJwtType.REFRESH,
+  } as const;
+}
